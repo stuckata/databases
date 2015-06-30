9d17da092cf9157b21accda178318ef95eb09956
stuckata <stu.kirkov@gmail.com>
2015-06-30 13:22:33 +0300

    problem 19

---
+++ 05_AdvancedSQL/Homework/19-Insert-Records.sql
@@ -0,0 +1,23 @@
+INSERT INTO Groups
+VALUES('Basic')
+
+INSERT INTO Groups
+VALUES('Fundamental')
+
+INSERT INTO Groups
+VALUES('Advanced')
+
+INSERT INTO Groups
+VALUES('Open Course')
+
+INSERT INTO Users
+VALUES('kircata', 'ggggtttt', 'Kiril Dobruschiev', 30-06-2015, 1)
+
+INSERT INTO Users
+VALUES('cookie', 'chiesecake', 'Sladka Brashnova', 29-06-2015, 2)
+
+INSERT INTO Users
+VALUES('zizi', 'zzzzzlk', 'Zaspala Lekinkova', 30-06-2015, 3)
+
+INSERT INTO Users
+VALUES('jimi', 'juju567', 'Djimi Sladoledov', 28-06-2015, 4)
