76b9082b33538d7ad686202054774e444e0ca846
stuckata <stu.kirkov@gmail.com>
2015-07-02 10:28:27 +0300

    problem 30

--- 05_AdvancedSQL/Homework/30-Insert-Update-Delete-WorkHours.sql
+++ 05_AdvancedSQL/Homework/30-Insert-Update-Delete-WorkHours.sql
@@ -1,9 +1,22 @@
 INSERT INTO WorkHours
-VALUES(6, )
+VALUES(6, CONVERT(DATE, GETDATE()), 'Create new DB', 1, 'Tables should have ...')
+
+INSERT INTO WorkHours
+VALUES(12, CONVERT(DATE, GETDATE()), 'New Interface...', 3, 'Should be implemented by...')
+
+INSERT INTO WorkHours
+VALUES(24, CONVERT(DATE, GETDATE()), 'New class...', 1, 'Should implement...')
+
+INSERT INTO WorkHours
+VALUES(56, CONVERT(DATE, GETDATE()), 'Should delete...', 1, 'Just do it...')
 
 UPDATE WorkHours
-SET Username='kica'
-WHERE Username='kichka'
+SET TaskDate = CONVERT(DATE, '2015-06-29')
+WHERE Id='2'
+
+UPDATE WorkHours
+SET TaskWorkHours = 4
+WHERE EmployeeId='24'
 
 DELETE FROM WorkHours
-WHERE Username='ginio'
+WHERE Id='4'
