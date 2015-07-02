ce916256d1c9743e614f480c134a786f15639da5
stuckata <stu.kirkov@gmail.com>
2015-07-02 18:21:15 +0300

    problem 31 bugs

--- 05_AdvancedSQL/Homework/31-WorkHoursLogs.sql
+++ 05_AdvancedSQL/Homework/31-WorkHoursLogs.sql
@@ -1,29 +1,31 @@
-CREATE TRIGGER WorkHoursLogs
+CREATE TRIGGER WorkHours_trigger
 ON WorkHours
 FOR INSERT, UPDATE, DELETE
 AS
-DECLARE @EmpID INT, @user VARCHAR(20), @activity VARCHAR(20);
+DECLARE @EmployeeId INT, @TaskDate DATE, @Task NVARCHAR(200), @TaskWorkHours INT, @Comments NVARCHAR(200), @Command NVARCHAR(20);
 
 IF EXISTS(SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
 BEGIN
-    SET @activity = 'UPDATE';
-    SET @user = SYSTEM_USER;
-    SELECT @EmpID = EmployeeID FROM inserted i;
-    INSERT INTO Emp_Audit(EmpID,Activity, DoneBy) VALUES (@EmpID,@activity,@user);
+    SET @Command = 'UPDATE';
+    SELECT @EmployeeId = EmployeeID FROM inserted i;
+	SELECT @TaskDate = TaskDate FROM inserted i;
+	SELECT @Task = Task FROM inserted i;
+	SELECT @TaskWorkHours = TaskWorkHours FROM inserted i;
+	SELECT @Comments = Comments FROM inserted i;
+	SELECT @Command = Command FROM inserted i;
+    INSERT INTO WorkHoursLogs(EmployeeID ,TaskDate, Task, TaskWorkHours, Comments, Command) VALUES (@EmployeeId, @TaskDate, @Task, @TaskWorkHours, @Comments, @Command);
 END
 
 IF EXISTS(SELECT * FROM inserted) AND NOT EXISTS(SELECT * FROM deleted)
 BEGIN
-    SET @activity = 'INSERT';
-    SET @user = SYSTEM_USER;
-    SELECT @EmpID = EmployeeID from inserted i;
-    INSERT into Emp_Audit(EmpID,Activity, DoneBy) values(@EmpID,@activity,@user);
+    SET @Command = 'INSERT';
+    SELECT @EmpID = EmployeeID FROM inserted i;
+    INSERT INTO WorkHoursLogs(EmpID,Activity, DoneBy) VALUES(@EmpID,@activity,@user);
 END
 
 IF EXISTS(SELECT * FROM deleted) AND NOT EXISTS(SELECT * FROM inserted)
 BEGIN 
-    SET @activity = 'DELETE';
-    SET @user = SYSTEM_USER;
-    SELECT @EmpID = EmployeeID from deleted i;
-    INSERT into Emp_Audit(EmpID,Activity, DoneBy) values(@EmpID,@activity,@user);
+    SET @Command = 'DELETE';
+    SELECT @EmpID = EmployeeID FROM deleted i;
+    INSERT INTO WorkHoursLogs(EmpID,Activity, DoneBy) VALUES(@EmpID,@activity,@user);
 END
