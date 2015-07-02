3f69c7659f26f3006584e68100ae5da5fe63fb14
stuckata <stu.kirkov@gmail.com>
2015-07-02 21:20:57 +0300

    problem 31

--- 05_AdvancedSQL/Homework/31-WorkHoursLogs.sql
+++ 05_AdvancedSQL/Homework/31-WorkHoursLogs.sql
@@ -1,31 +1,30 @@
-CREATE TRIGGER WorkHours_trigger
-ON WorkHours
-FOR INSERT, UPDATE, DELETE
+CREATE TABLE WorkHoursLogs
+	(
+		Id int NOT NULL PRIMARY KEY IDENTITY,
+		WorkHoursId int NOT NULL, 	
+		OldRecord nvarchar(max),
+		NewRecord nvarchar(max),
+		CommandType nvarchar(20)
+	)
+GO
+
+CREATE TRIGGER WorkHoursUpdate_trigger ON WorkHours FOR UPDATE
 AS
-DECLARE @EmployeeId INT, @TaskDate DATE, @Task NVARCHAR(200), @TaskWorkHours INT, @Comments NVARCHAR(200), @Command NVARCHAR(20);
+	INSERT INTO WorkHoursLogs(WorkHoursId, OldRecord, NewRecord, CommandType)
+	SELECT i.Id, d.Task, i.Task, 'UPDATE'
+	FROM deleted d, inserted i
+GO
 
-IF EXISTS(SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
-BEGIN
-    SET @Command = 'UPDATE';
-    SELECT @EmployeeId = EmployeeID FROM inserted i;
-	SELECT @TaskDate = TaskDate FROM inserted i;
-	SELECT @Task = Task FROM inserted i;
-	SELECT @TaskWorkHours = TaskWorkHours FROM inserted i;
-	SELECT @Comments = Comments FROM inserted i;
-	SELECT @Command = Command FROM inserted i;
-    INSERT INTO WorkHoursLogs(EmployeeID ,TaskDate, Task, TaskWorkHours, Comments, Command) VALUES (@EmployeeId, @TaskDate, @Task, @TaskWorkHours, @Comments, @Command);
-END
+CREATE TRIGGER WorkHoursDelete_trigger ON WorkHours FOR DELETE
+AS
+	INSERT INTO WorkHoursLogs(WorkHoursId, OldRecord, NewRecord, CommandType)
+	SELECT d.Id, d.Task, NULL, 'DELETE'
+	FROM deleted d
+GO
 
-IF EXISTS(SELECT * FROM inserted) AND NOT EXISTS(SELECT * FROM deleted)
-BEGIN
-    SET @Command = 'INSERT';
-    SELECT @EmpID = EmployeeID FROM inserted i;
-    INSERT INTO WorkHoursLogs(EmpID,Activity, DoneBy) VALUES(@EmpID,@activity,@user);
-END
-
-IF EXISTS(SELECT * FROM deleted) AND NOT EXISTS(SELECT * FROM inserted)
-BEGIN 
-    SET @Command = 'DELETE';
-    SELECT @EmpID = EmployeeID FROM deleted i;
-    INSERT INTO WorkHoursLogs(EmpID,Activity, DoneBy) VALUES(@EmpID,@activity,@user);
-END
+CREATE TRIGGER WorkHoursInsert_trigger ON WorkHours FOR INSERT
+AS
+	INSERT INTO WorkHoursLogs(WorkHoursId, OldRecord, NewRecord, CommandType)
+	SELECT i.Id, NULL, i.Task, 'INSERT'
+	FROM inserted i
+GO
