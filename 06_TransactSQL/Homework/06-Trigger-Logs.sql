29decbd650349bd7c9cd08cb5a03c993ccf52761
stuckata <stu.kirkov@gmail.com>
2015-07-03 18:19:41 +0300

    problem 06 not ready

---
+++ 06_TransactSQL/Homework/06-Trigger-Logs.sql
@@ -0,0 +1,19 @@
+USE TestSP
+
+CREATE TABLE Logs
+(
+Id INT NOT NULL IDENTITY PRIMARY KEY,
+AccountId INT NOT NULL FOREIGN KEY REFERENCES Accounts(Id),
+OldSum MONEY NOT NULL,
+NewSum MONEY NOT NULL,
+)
+GO
+
+CREATE TRIGGER tr_AccountBalanceUpdate
+ON Accounts
+FOR UPDATE
+AS
+	INSERT INTO Logs(AccountId, OldSum, NewSum)
+	SELECT i.Id, d.Balance, i.Balance
+	FROM deleted d, inserted i
+GO
