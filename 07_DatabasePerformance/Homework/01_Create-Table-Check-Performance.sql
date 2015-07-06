4b1c2ed5700883195bc5dfe2405ea3708a33d36e
stuckata <stu.kirkov@gmail.com>
2015-07-06 17:53:56 +0300

    problem 01 random date and random string

--- 07_DatabasePerformance/Homework/01_Create-Table-Check-Performance.sql
+++ 07_DatabasePerformance/Homework/01_Create-Table-Check-Performance.sql
@@ -15,35 +15,6 @@ CREATE TABLE TestData
 	)
 GO
 
-CREATE FUNCTION ufn_GenerateRandomString()
-RETURNS NVARCHAR(50)
-AS
-BEGIN
-	DECLARE @lenght INT
-	DECLARE @charPool NVARCHAR(100)
-	DECLARE @poolLength INT
-	DECLARE @loopCount INT
-	DECLARE @randomString NVARCHAR
+SELECT SUBSTRING(CONVERT(NVARCHAR(50), NEWID()),0,9)
 
-	-- min_length = 8, max_length = 12
-	SET @lenght = RAND() * 5 + 8
-	-- SET @Length = RAND() * (max_length - min_length + 1) + min_length
-
-	-- define allowable character explicitly - easy to read this way an easy to 
-	-- omit easily confused chars like l (ell) and 1 (one) or 0 (zero) and O (oh)
-	SET @charPool = 
-		'abcdefghijkmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ23456789.,-_!$@#%^&*'
-	SET @poolLength = Len(@charPool)
-
-	SET @loopCount = 0
-	SET @randomString = ''
-
-	WHILE (@loopCount < @lenght) BEGIN
-		SELECT @randomString = @randomString + 
-			SUBSTRING(@charPool, CONVERT(INT, RAND() * @poolLength), 1)
-		SELECT @loopCount = @loopCount + 1
-	END
-
-	RETURN @randomString
-END
-GO
+SELECT DATEADD(DAY, (ABS(CHECKSUM(NEWID())) % 5475), '2000-01-01')
