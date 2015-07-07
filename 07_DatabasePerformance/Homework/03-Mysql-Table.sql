b5ef8e71892597632b84335d941a997d66dbf5c7
stuckata <stu.kirkov@gmail.com>
2015-07-07 23:03:32 +0300

    problem 03 not solved

--- 07_DatabasePerformance/Homework/03-Mysql-Table.sql
+++ 07_DatabasePerformance/Homework/03-Mysql-Table.sql
@@ -1,26 +1,23 @@
-CREATE DATABASE test_performance;
+CREATE TABLE tests_data (
+    Id INT NOT NULL AUTO_INCREMENT,
+    TestDate DATE NOT NULL,
+    TestText NVARCHAR(20) NOT NULL,
+    PRIMARY KEY (Id)
+);
 
-USE test_performance;
 
-CREATE TABLE test_data
-	(
-		Id INT NOT NULL AUTO_INCREMENT,
-		TestDate DATE NOT NULL,
-		TestText NVARCHAR(20) NOT NULL,
-        PRIMARY KEY(Id)
-    );
-    
-USE test_data;
- 
 BEGIN
-	DECLARE rows INT = 10000000
-    
-	WHILE rows > 0 DO
-	BEGIN
-		DECLARE test_date DATE = DATEADD(DAY, (ABS(CHECKSUM(NEWID())) % 5475), '2000-01-01')
-		DECLARE test_text NVARCHAR(20) = SUBSTRING(CONVERT(NVARCHAR(50), NEWID()),0,9)
-		INSERT INTO TestData
-		VALUES(test_date, test_text)
-		SET rows = rows - 1
-	END WHILE;
-END;
+	DECLARE rows_num INT DEFAULT 10000000
+	SET rows_num = 10000000
+
+	WHILE rowsNum > 0 DO
+			BEGIN
+				DECLARE test_date DATE = DATEADD(DAY, (ABS(CHECKSUM(NEWID())) % 5475), '2000-01-01')
+				DECLARE test_text NVARCHAR(20) = SUBSTRING(CONVERT(NVARCHAR(50), NEWID()),0,9)
+				
+				INSERT INTO tests_data
+				VALUES(test_date, test_text)
+				SET rows_num = rows_num - 1
+			END
+	END WHILE
+END
